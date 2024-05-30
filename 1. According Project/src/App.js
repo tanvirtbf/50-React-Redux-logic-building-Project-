ffb36414407d2968cab5a003618d73438c2fada4
tanvirtbf <tanver018765@gmail.com>
2024-05-30 11:19:47 +0600

    functional logic complete

--- 1. According Project/src/App.js
+++ 1. According Project/src/App.js
@@ -1,14 +1,12 @@
-import { useState } from "react";
 import "./App.css";
 import Accordian from "./components/accordian";
 
 function App() {
-  const [selected,setSelected] = useState(null)
   return (
     <div className="App">
       <Accordian />
     </div>
-  )
+  );
 }
 
 export default App;
--- 1. According Project/src/components/accordian/index.jsx
+++ 1. According Project/src/components/accordian/index.jsx
@@ -1,16 +1,33 @@
+import { useState } from "react";
+import { FaPlus } from "react-icons/fa";
 import data from "./data";
 
 export default function Accordian() {
+  const [selected, setSelected] = useState(null);
+  const handleSingleSelection = (getCurrentId) => {
+    setSelected(getCurrentId === selected ? null : getCurrentId)
+  };
+  console.log(selected)
   return (
     <div className="wrapper">
       <div className="accordian">
         {data && data.length > 0 ? (
           data.map((dataItem) => (
-            <div className="title">
-              <div className="title">
+            <div className="item" key={dataItem.id}>
+              <div
+                onClick={() => handleSingleSelection(dataItem.id)}
+                className="title"
+              >
                 <h3>{dataItem.question}</h3>
-                <span>+</span>
+                <span>
+                  <FaPlus />
+                </span>
               </div>
+              {
+                selected === dataItem.id ?   
+                <div className="content">{dataItem.answer}</div>
+                : null
+              }
             </div>
           ))
         ) : (
