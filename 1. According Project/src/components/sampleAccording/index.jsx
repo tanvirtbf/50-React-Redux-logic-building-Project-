1452e3714c8b518e84a535034cc96f1806229d06
tanvirtbf <tanver018765@gmail.com>
2024-06-02 10:42:55 +0600

    basic ui complete

--- 1. According Project/src/components/sampleAccording/index.jsx
+++ 1. According Project/src/components/sampleAccording/index.jsx
@@ -3,7 +3,22 @@ export default function SampleAccordian(){
 
   return(
     <div className="main">
-      <h1>Hello</h1>
+      <div className='row'>
+        <div className='accordian-btn'>
+          <button>Enable Multi Selection</button>
+        </div>
+        <div className='accordian-item'>
+          <div className='items'>
+            <div className='items-heading'>
+              <h2>Hello</h2>
+              <span>Icon</span>
+            </div>
+            <div className='items-text'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, explicabo.
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
