b8deda9d3fd8244b2bd89591d4e376304db449e0
tanvirtbf <tanver018765@gmail.com>
2024-06-02 16:12:14 +0600

    build rgb logic

--- 2. Random Color/src/components/random-color/index.js
+++ 2. Random Color/src/components/random-color/index.js
@@ -5,41 +5,52 @@ const RandomColor = () => {
   const [colorType, setColorType] = useState("hex");
   const [color, setColor] = useState("#000000");
 
-  function randomHex(length){
-    return Math.floor(Math.random()*length)
+  function randomHex(length) {
+    return Math.floor(Math.random() * length);
   }
 
   const generateHexColor = () => {
-    const hex = [1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
-    let hexColor = '#';
+    const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+    let hexColor = "#";
 
-    for(let i = 0; i<6; i++){
-      hexColor += hex[randomHex(hex.length)]
+    for (let i = 0; i < 6; i++) {
+      hexColor += hex[randomHex(hex.length)];
     }
-    setColor(hexColor)
+    setColor(hexColor);
   };
   const generateRgbColor = () => {
-
+    const r = randomHex(255);
+    const g = randomHex(255);
+    const b = randomHex(255);
+    setColor(`rgb(${r},${g},${b})`);
+    console.log(color);
   };
 
   return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        background: color,
-      }}
-    >
-      <button onClick={() => setColorType("hex")}>Create HEX Color</button>
-      <button onClick={() => setColorType("rgb")}>Create RGB Color</button>
-      <button
-        onClick={
-          colorType === "hex" ? generateHexColor : generateRgbColor
-        }
+    <>
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          background: color,
+        }}
       >
-        Generate Random Color
-      </button>
-    </div>
+        <button onClick={() => setColorType("hex")}>Create HEX Color</button>
+        <button onClick={() => setColorType("rgb")}>Create RGB Color</button>
+        <button
+          onClick={colorType === "hex" ? generateHexColor : generateRgbColor}
+        >
+          Generate Random Color
+        </button>
+        <div>
+          {colorType === "hex" ? (
+            <h3>hex value is ${color}</h3>
+          ) : (
+            <h3>rgb value is ${color}</h3>
+          )}
+        </div>
+      </div>
+    </>
   );
 };
 
