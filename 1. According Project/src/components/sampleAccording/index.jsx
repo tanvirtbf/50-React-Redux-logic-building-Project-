bd72678482e731e644640592e813ea05dde0db76
tanvirtbf <tanver018765@gmail.com>
2024-06-02 10:49:24 +0600

    basic css implement and import icon from react icon library for arrow

--- 1. According Project/src/components/sampleAccording/index.jsx
+++ 1. According Project/src/components/sampleAccording/index.jsx
@@ -1,4 +1,8 @@
 import './styles.css'
+import { FaArrowAltCircleUp } from "react-icons/fa";
+import { FaArrowAltCircleDown } from "react-icons/fa";
+
+
 export default function SampleAccordian(){
 
   return(
@@ -8,6 +12,33 @@ export default function SampleAccordian(){
           <button>Enable Multi Selection</button>
         </div>
         <div className='accordian-item'>
+          <div className='items'>
+            <div className='items-heading'>
+              <h2>Hello</h2>
+              <span>Icon</span>
+            </div>
+            <div className='items-text'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, explicabo.
+            </div>
+          </div>
+          <div className='items'>
+            <div className='items-heading'>
+              <h2>Hello</h2>
+              <span>Icon</span>
+            </div>
+            <div className='items-text'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, explicabo.
+            </div>
+          </div>
+          <div className='items'>
+            <div className='items-heading'>
+              <h2>Hello</h2>
+              <span>Icon</span>
+            </div>
+            <div className='items-text'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, explicabo.
+            </div>
+          </div>
           <div className='items'>
             <div className='items-heading'>
               <h2>Hello</h2>
