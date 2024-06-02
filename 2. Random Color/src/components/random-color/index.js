d62db52aa767b1a809c545059f4fc1b48e14ed7e
tanvirtbf <tanver018765@gmail.com>
2024-06-02 11:57:49 +0600

    Random color project start and complete routing

---
+++ 2. Random Color/src/components/random-color/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.css'
+
+const RandomColor = () => {
+  return (
+    <div>
+      hello random-color
+    </div>
+  )
+}
+
+export default RandomColor
---
+++ 2. Random Color/src/index.js
@@ -0,0 +1,34 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Home from "./components/home";
+import RandomColor from "./components/random-color";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/random-color",
+        element: <RandomColor />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
