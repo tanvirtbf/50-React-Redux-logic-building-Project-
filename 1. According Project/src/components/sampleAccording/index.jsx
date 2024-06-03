9f0ed5cd4efa798fb38c6c565867582517010861
tanvirtbf <tanver018765@gmail.com>
2024-06-03 09:27:58 +0600

    again alone this project accordian

--- 1. According Project/src/components/sampleAccording/index.jsx
+++ 1. According Project/src/components/sampleAccording/index.jsx
@@ -1,66 +1,23 @@
-import { useState } from "react";
-import { FaArrowAltCircleDown, FaArrowAltCircleUp } from "react-icons/fa";
-import data from "./data";
 import "./styles.css";
 
 export default function SampleAccordian() {
-  const [selected, setSelected] = useState(null);
-  const [isEnable, setIsEnable] = useState(false);
-  const [multiple, setMultiple] = useState([]);
-
-  const singleClick = (id) => {
-    setSelected(selected === id ? null : id);
-  };
-  const doubleClick = (id) => {
-    const multiArr = [...multiple];
-    const searchIndex = multiArr.indexOf(id);
-    if (searchIndex === -1) multiArr.push(id);
-    else multiArr.splice(searchIndex, 1);
-    setMultiple(multiArr);
-    console.log(multiArr);
-  };
-
   return (
     <div className="main">
       <div className="row">
         <div className="accordian-btn">
-          <button onClick={() => setIsEnable(!isEnable)}>
-            {isEnable ? "Disable" : "Enable"} Multi Selection
-          </button>
+          <button>Multi Selection</button>
         </div>
         <div className="accordian-item">
-          {data && data.length > 0 ? (
-            data.map((item) => (
-              <div className="items" key={item.id}>
-                <div
-                  className="items-heading"
-                  onClick={() =>
-                    isEnable ? doubleClick(item.id) : singleClick(item.id)
-                  }
-                >
-                  <h2>{item.question}</h2>
-                  <span>
-                    {selected === item.id ? (
-                      <FaArrowAltCircleDown className="arrow" />
-                    ) : (
-                      <FaArrowAltCircleUp className="arrow" />
-                    )}
-                  </span>
-                </div>
-                <div className="items-text">
-                  {isEnable
-                    ? multiple.indexOf(item.id) !== -1 && (
-                        <div className="content">{item.answer}</div>
-                      )
-                    : selected === item.id && (
-                        <div className="content">{item.answer}</div>
-                      )}
-                </div>
-              </div>
-            ))
-          ) : (
-            <h1>Data not found</h1>
-          )}
+          <div className="items">
+            <div className="items-heading">
+              <h2>question</h2>
+              <span>icon</span>
+            </div>
+            <div className="items-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+              incidunt!
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- 2. Random Color/src/components/star-rating/index.js
+++ 2. Random Color/src/components/star-rating/index.js
@@ -1,8 +1,14 @@
-export default function StarRating(){
+import { FaStar } from "react-icons/fa";
+import "./styles.css";
+import { useState } from "react";
+export default function StarRating({ noOfStars = 5 }) {
+  const [rating,setRating] = useState(0)
 
   return (
-    <>
-      hello Ratings
-    </>
-  )
+    <div className="star-rating">
+      {[...Array(noOfStars)].map((_, index) => {
+        return <FaStar key={index} onClick={} onMouseMove={} onMouseLeave={} size={40}/>;
+      })}
+    </div>
+  );
 }
