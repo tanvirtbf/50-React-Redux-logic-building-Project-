cc1191ee0e733cc2180e74733beeaecf5748ce11
tanvirtbf <tanver018765@gmail.com>
2024-06-03 14:13:44 +0600

    start slider project

---
+++ 1. According Project/src/components/image-slider/index.jsx
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+import { useState } from "react"
+
+export default function ImageSlider({url,limit}){
+  const [images,setImages] = useState([])
+  const [currentSlide,setCurrentSlide] = useState(0);
+  const [errorMsg,setErrorMsg] = useState(null)
+
+  async function fetchImages(getUrl){
+
+    try {
+      const response = await fetch(getUrl)
+      const data = await response.json();
+
+      if(data){
+        setImages(data)
+      }
+    } catch (e) {
+      setErrorMsg(e.message)
+    }
+  }
+
+  useEffect(()=>{
+    if(url !== ''){
+      fetchImages(url)
+    }
+  },[url])
+
+  return (
+    <>
+      <div className="container">
+
+      </div>
+    </>
+  )
+}
