61d0e4854e6433642b6e069d4d5054b9e6ed92de
tanvirtbf <tanver018765@gmail.com>
2024-06-03 14:18:11 +0600

    loading state managed and setup logic

--- 1. According Project/src/components/image-slider/index.jsx
+++ 1. According Project/src/components/image-slider/index.jsx
@@ -4,19 +4,23 @@ import { useState } from "react"
 export default function ImageSlider({url,limit}){
   const [images,setImages] = useState([])
   const [currentSlide,setCurrentSlide] = useState(0);
-  const [errorMsg,setErrorMsg] = useState(null)
+  const [errorMsg,setErrorMsg] = useState(null);
+  const [loading,setLoading] = useState(false)
 
   async function fetchImages(getUrl){
 
     try {
-      const response = await fetch(getUrl)
+      setLoading(true)
+      const response = await fetch(getUrl);
       const data = await response.json();
 
       if(data){
         setImages(data)
+        setLoading(false)
       }
     } catch (e) {
       setErrorMsg(e.message)
+      setLoading(false)
     }
   }
 
