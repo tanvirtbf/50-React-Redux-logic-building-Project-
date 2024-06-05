fc6e3a3867f51c24d4c19da151dd0d07146680f7
tanvirtbf <tanver018765@gmail.com>
2024-06-05 15:30:21 +0600

    start load more data project

---
+++ 4. Load More Project/src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Title from '../title/Title'
+
+const Home = () => {
+  return (
+    <>
+      <Title title={"1. Accordian Project"}/>
+    </>
+  )
+}
+
+export default Home
---
+++ 4. Load More Project/src/index.js
@@ -0,0 +1,30 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Home from "./components/home/Home";
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
