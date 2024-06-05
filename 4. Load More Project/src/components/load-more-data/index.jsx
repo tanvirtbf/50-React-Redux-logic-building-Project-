10b0f02857355414822929902eb40d7b76e8a365
tanvirtbf <tanver018765@gmail.com>
2024-06-05 16:58:11 +0600

    products mapping start

--- 4. Load More Project/src/components/load-more-data/index.jsx
+++ 4. Load More Project/src/components/load-more-data/index.jsx
@@ -7,20 +7,38 @@ function LoadMore() {
 
   async function fetchProducts(){
     try {
+      setLoading(true)
       const res = await fetch(`https://dummyjson.com/products?limit=20&skip=${count === 0 ? 0 : count * 20}`)
       const result = await res.json();
       console.log(result)
+
+      if(result && result.products && result.products.length){
+        setProducts(result.products)
+        setLoading(false)
+      }
+
     } catch (e) {
       console.log(e)
+      setLoading(false)
     }
   }
   useEffect(()=>{
     fetchProducts()
   },[])
 
+  if(loading){
+    return <h1>Loading data...</h1>
+  }
+
   return (
     <div className='container'>
-      lore more data
+      <div>
+        {
+          products && products.length ? products.map((item) => <div key={item.id}>
+
+          </div>) : null
+        }
+      </div>
     </div>
   )
 }
